2dfffbd2d2bf9263b2ceb4543a43fcbd826c37e7
amerelsayed1 <amer.mahsoub1@gmail.com>
2025-01-11 08:14:55 +0200

    Fix linting issues

--- src/main/java/io/amermahsoub/ecommerce_store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/amermahsoub/ecommerce_store/exceptions/GlobalExceptionHandler.java
@@ -1,21 +1,16 @@
 package io.amermahsoub.ecommerce_store.exceptions;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
